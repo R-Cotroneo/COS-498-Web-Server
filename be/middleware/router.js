915b2f69d393c4213dc4f2dfef251479f374afe7
Raist <vincent.cotroneo@maine.edu>
2025-12-08 18:14:27 +0000

    chore: Initial setup of packages and routes

--- be/middleware/router.js
+++ be/middleware/router.js
@@ -7,4 +7,24 @@ router.get("/", (req, res) => {
     res.render("home");
 });
 
+// Register route
+router.get("/register", (req, res) => {
+    res.render("register");
+});
+
+// Login route
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
+// Comments route
+router.get("/comments", (req, res) => {
+    res.render("comments");
+});
+
+// New Comment route
+router.get("/comment/new", (req, res) => {
+    res.render("new-comment");
+});
+
 module.exports = router;
